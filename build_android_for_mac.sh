963196da7a3553fa1256b7d1417cd205716b3faf
Giggle Liao <giggle@zillians.com>
2014-07-02 11:39:23 +0800

    modify install instruction and build file for mac

---
+++ build_android_for_mac.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+SYSROOT=$NDK/platforms/android-9/arch-arm/
+TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.8/prebuilt/darwin-x86_64
+FFMPEG_OUTPUT_SOURCES=$NDK/sources/ffmpeg
+function build_one
+{
+./configure \
+    --prefix=$PREFIX \
+    --enable-shared \
+    --disable-static \
+    --disable-doc \
+    --disable-ffmpeg \
+    --disable-ffplay \
+    --disable-ffprobe \
+    --disable-ffserver \
+    --disable-avdevice \
+    --disable-doc \
+    --disable-symver \
+    --cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+    --target-os=linux \
+    --arch=arm \
+    --enable-cross-compile \
+    --sysroot=$SYSROOT \
+    --extra-cflags="-Os -fpic $ADDI_CFLAGS" \
+    --extra-ldflags="$ADDI_LDFLAGS" \
+    $ADDITIONAL_CONFIGURE_FLAG
+make clean
+make -j4
+make install
+}
+CPU=arm
+PREFIX=$(pwd)/android/$CPU 
+ADDI_CFLAGS="-marm"
+build_one
+
+# make a symbolic link to the current source folder
+ln -f -s `pwd` $FFMPEG_OUTPUT_SOURCES
+cp Android.mk $PREFIX
